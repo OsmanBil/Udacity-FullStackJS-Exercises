35f45bb46b87e8c384de5e61c9706c6969d4947e
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-07-07 19:18:47 +0200

    update

--- GettingStartedWithNodeJs/Developing-With-Typescript/object-Like-Types.ts
+++ GettingStartedWithNodeJs/Developing-With-Typescript/object-Like-Types.ts
@@ -1,76 +1,76 @@
-// Object 
-let student: { name: string, age: number, enrolled: boolean } = { name: 'Maria', age: 10, enrolled: true };
+// // Object 
+// let student: { name: string, age: number, enrolled: boolean } = { name: 'Maria', age: 10, enrolled: true };
 
-// interface
-interface Student {
-    name: string,
-    age: number,
-    enrolled: boolean
-};
-let newStudent: Student = { name: 'Maria', age: 10, enrolled: true };
+// // interface
+// interface Student {
+//     name: string,
+//     age: number,
+//     enrolled: boolean
+// };
+// let newStudent: Student = { name: 'Maria', age: 10, enrolled: true };
 
-// Optional and Readonly Properties
+// // Optional and Readonly Properties
 
-interface Student {
-    name: string,
-    age: number,
-    enrolled: boolean,
-    phone?: number // phone becomes optional
-};
+// interface Student {
+//     name: string,
+//     age: number,
+//     enrolled: boolean,
+//     phone?: number // phone becomes optional
+// };
 
-interface Student {
-    name: string,
-    age: number,
-    enrolled: boolean,
-    readonly id: number // id is readonly
-};
+// interface Student {
+//     name: string,
+//     age: number,
+//     enrolled: boolean,
+//     readonly id: number // id is readonly
+// };
 
 
-  // Type Aliases
-  type Student = { 
-    name: string; 
-    age: number;
-    enrolled: boolean;
-  };
+//   // Type Aliases
+//   type Student = { 
+//     name: string; 
+//     age: number;
+//     enrolled: boolean;
+//   };
   
-  let newStudent:Student = {name: 'Maria', age: 10, enrolled: true};
+//   let newStudent:Student = {name: 'Maria', age: 10, enrolled: true};
 
-  // Classes
+//   // Classes
 
-  class Student {
-    studentGrade: number;
-    studentId: number;
-    constructor(grade: number, id: number) {
-      this.studentGrade = grade;
-      this.studentId = id;
-    }
-  }
+//   class Student {
+//     studentGrade: number;
+//     studentId: number;
+//     constructor(grade: number, id: number) {
+//       this.studentGrade = grade;
+//       this.studentId = id;
+//     }
+//   }
 
-  // Access Modifiers
-  class Student {
-    protected studentGrade: number;
-    private studentId: number;
-    public constructor(grade: number, id: number) {
-      this.studentGrade = grade;
-      this.studentId = id;
-    }
-    id(){
-      return this.studentId;
-    }
-  }
+//   // Access Modifiers
+//   class Student {
+//     protected studentGrade: number;
+//     private studentId: number;
+//     public constructor(grade: number, id: number) {
+//       this.studentGrade = grade;
+//       this.studentId = id;
+//     }
+//     id(){
+//       return this.studentId;
+//     }
+//   }
   
-  class Graduate extends Student {
-    studentMajor: string; // public by default
-    public constructor(grade: number, id: number, major: string ){
-        super(grade, id);
-        this.studentId = id; // TypeScript Error: Property 'studentId' is private and only accessible within class 'Student'.
-        this.studentGrade = grade; // Accessable because parent is protected
-        this.studentMajor = major;
-    }
-  }
+//   class Graduate extends Student {
+//     studentMajor: string; // public by default
+//     public constructor(grade: number, id: number, major: string ){
+//         super(grade, id);
+//         this.studentId = id; // TypeScript Error: Property 'studentId' is private and only accessible within class 'Student'.
+//         this.studentGrade = grade; // Accessable because parent is protected
+//         this.studentMajor = major;
+//     }
+//   }
   
-  const myStudent = new Graduate(3, 1234, 'computer science');
+//   const myStudent = new Graduate(3, 1234, 'computer science');
   
-  console.log(myStudent.id()); //  prints 1234
-  myStudent.studentId = 1235; // TypeScript Error: Property 'studentId' is private and only accessible within class 'Student'.
-  console.log(myStudent.id()); // prints 1235
+//   console.log(myStudent.id()); //  prints 1234
+//   myStudent.studentId = 1235; // TypeScript Error: Property 'studentId' is private and only accessible within class 'Student'.
+//   console.log(myStudent.id()); // prints 1235
